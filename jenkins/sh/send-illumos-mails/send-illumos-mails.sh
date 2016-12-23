ad2b0a70fe6a5a7e6925b87a8f808cf14c9fa855
Prakash Surya <prakash.surya@delphix.com>
2016-12-23 13:01:00 -0800

    Incremental progress over the initial version
     - The use of "Net::SMTP" is replaced with "Mail"
     - The "send illumos mail" label has been added to the workflow
     - The "[REVIEW]" subject prefix has been removed
     - GMail is now used to used mails, instead of Delphix's SMTP server
     - UTF-8 is explicitly used to encode the sent mail

--- jenkins/sh/send-illumos-mails/send-illumos-mails.sh
+++ jenkins/sh/send-illumos-mails/send-illumos-mails.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source "${SH_LIBRARY_PATH}/common.sh"
+source "${SH_LIBRARY_PATH}/vault.sh"
 source "${SH_LIBRARY_PATH}/githubapi.sh"
 
 check_env REPOSITORY
@@ -10,8 +11,14 @@ NAME=$(basename -s ".sh" ${BASH_SOURCE[0]})
 
 githubapi_setup_environment
 
-log_must ruby "${DIR}/${NAME}.rb" \
+#
+# To avoid the password being present in the Jenkins job console page,
+# we pass the SMTP password to the ruby script via the processes stdin.
+#
+echo $(vault_read_smtp_password) | log_must ruby "${DIR}/${NAME}.rb" \
     --netrc-file netrc-file \
     --repository "$REPOSITORY" \
+    --smtp-user "$(vault_read_smtp_user)" \
+    --smtp-password "-"
 
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab textwidth=72 colorcolumn=80
